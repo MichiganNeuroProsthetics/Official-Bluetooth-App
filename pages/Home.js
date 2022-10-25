66d12487302588d98722872063baa8dead76ca4a
erjiayu <erjiayu@umich.edu>
2022-10-25 17:05:30 -0400

    Test.

--- pages/Home.js
+++ pages/Home.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 import Header from "../components/Header"
 import TabBar from '../components/TabBar'
