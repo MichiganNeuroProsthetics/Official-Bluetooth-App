40b9e6c53feba60be6a503c3519cf5be2f37014e
kxlee <kxlee@umich.edu>
2022-10-26 19:42:04 -0400

    header update

--- App.js
+++ App.js
@@ -22,8 +22,6 @@ export default function App() {
   return (
     <NavigationContainer>
        <Tab.Navigator
-
-
         initialRouteName={homeName}
         screenOptions={({ route }) => ({
           headerStyle: {
