f35d74e1f266afb9894b4839e7b6b66dbcb6d12f
kxlee <kxlee@umich.edu>
2022-11-02 02:48:10 -0400

    background complete

--- pages/Home.js
+++ pages/Home.js
@@ -3,22 +3,14 @@ import { View, Text, StyleSheet, SafeAreaView, Image, ImageBackground } from 're
 import Header from "../components/Header"
 import TabBar from '../components/TabBar'
 import { Divider } from 'react-native-elements'
-const background = {uri:"https://raw.githubusercontent.com/erjiayu/MNP-App/main/og-mosaicgradient.png"}
-// import background from '../constants/images'
+const background = {uri:"https://raw.githubusercontent.com/MichiganNeuroProsthetics/Official-Bluetooth-App/main/assets/mosaic.png"}
 const Home = () => {
   return (
-    // <SafeAreaView style = {{alignItem: 'center', justifyContent: 'center'}}>
-      <ImageBackground 
-        source={background}
-        resizeMode="cover"
-        style = {styles.image}>
-        <View >
-          {/* <Text style = {styles.title}>
-            Welcome Back, User
-          </Text> */}
-        </View>
-      </ImageBackground>
-    // </SafeAreaView>
+    <SafeAreaView style = {{alignItem: 'center', justifyContent: 'center'}}>
+      <Text style = {styles.title}>
+        Welcome Back, User
+      </Text>
+    </SafeAreaView>
   )
 }
 
@@ -27,9 +19,9 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     justifyContent: "center",
-    width: "130%",
-    height: "130%",
-    transform: [{ rotate: '45deg' }]
+    width: "100%",
+    height: "100%",
+    // transform: [{ rotate: '45deg' }]
   }, 
   title: {
     fontSize: 26,
