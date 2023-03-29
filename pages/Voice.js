975c74c07e18a8532370989b54aa40527c742ffd
kxlee <kxlee@umich.edu>
2023-03-29 17:55:24 -0400

    voice plugin

---
+++ pages/Voice.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Voice, {
+    SpeechRecognizedEvent,
+    SpeechResultsEvent,
+    SpeechErrorEvent,
+  } from "@react-native-voice/voice";
+
+const Voice = () => {
+  return (
+    <div>Voice</div>
+  )
+}
+
+export default Voice
