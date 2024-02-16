28a6e9b2550d8d52123391911533af2c7e06dfa6
kxlee <kxlee@umich.edu>
2024-02-16 16:58:07 -0500

    User page WIP

--- pages/User.js
+++ pages/User.js
@@ -1,13 +1,34 @@
 import React from 'react'
-import { View, Text, SafeAreaView  } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet, Dimensions} from 'react-native'
+
+const {width, height} = Dimensions.get('screen')
+const WIDTH = width
+const HEIGHT = height
 
 const User = () => {
   return (
-    <SafeAreaView>
-        <Text>User</Text>
-    </SafeAreaView>
-    
+    <View>
+
+      <View style={styles.infoCard}>
+        <Text>Name</Text>
+      </View>
+
+    </View>
+
   )
 }
+
+const styles = StyleSheet.create({
+  infoCard: {
+    borderRadius: 50,
+    backgroundColor: 'rgba(255, 255, 255, 0.6)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: WIDTH,
+    height: HEIGHT * 0.6,
+    top: HEIGHT * 0.2,
+  }
+})
 
 export default User
